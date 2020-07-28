2e97c4390254aa15a308baafd3688c6db7f7edc3
robloxro <ruxi.jitianu@gmail.com>
2020-07-28 19:20:56 +0300

    Initial commit

---
+++ src/test/java/com/robloxro/cardReader/dataConsumer/DataConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.robloxro.cardReader.dataConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
